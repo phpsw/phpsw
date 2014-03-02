90fc0b8dc59330acf28ccd01aa615bbd983948da
Steve Lacey <steve@stevelacey.net>
2014-03-02 20:30:43 +0000

    Meetup API abstraction.

---
+++ src/API/Meetup.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace PHPSW\API;
+
+use DMS\Service\Meetup\MeetupKeyAuthClient;
+
+class Meetup
+{
+    protected $client;
+
+    protected $config;
+
+    protected $events;
+
+    public function __construct(array $config)
+    {
+        $this->client = MeetupKeyAuthClient::factory(['key' => $config['api']['key']]);
+        $this->config = $config;
+    }
+
+    public function getEvents()
+    {
+        if ($this->events === null) {
+            $events = $this->client->getEvents([
+                'group_urlname' => $this->config['urlname'],
+                'status' => implode(',', [
+                    'upcoming', 'past', 'proposed', 'suggested', 'cancelled'
+                ]),
+                'desc' => 'true'
+            ]);
+
+            $this->events = array_map(
+                function ($event) {
+                    $event = (object) $event;
+
+                    $event->date = \DateTime::createFromFormat('U', $event->time / 1000);
+                    $event->url = $event->event_url;
+                    $event->venue = (object) $event->venue;
+
+                    return $event;
+                },
+                iterator_to_array($events)
+            );
+        }
+
+        return $this->events;
+    }
+
+    public function getPastEvents()
+    {
+        return array_filter($this->getEvents(), function ($event) {
+            return $event->status == 'past';
+        });
+    }
+
+    public function getUpcomingEvents()
+    {
+        return array_filter($this->getEvents(), function ($event) {
+            return $event->status == 'upcoming';
+        });
+    }
+
+    public function getDiscussionBoard()
+    {
+        return current($this->getDiscussionBoards());
+    }
+
+    public function getDiscussionBoards()
+    {
+        $boards = $this->client->getDiscussionBoards([
+            'urlname' => $this->config['urlname']
+        ]);
+
+        return array_map(
+            function ($board) {
+                return (object) $board;
+            },
+            $boards->getData()
+        );
+    }
+
+    public function getPosts($board = null)
+    {
+        if ($board === null) {
+            $board = $this->getDiscussionBoard();
+        }
+
+        $posts = $this->client->getDiscussions([
+            'urlname' => $this->config['urlname'],
+            'bid' => $board->id
+        ]);
+
+        return array_map(
+            function ($post) {
+                $post = (object) $post;
+
+                $post->last_post = (object) $post->last_post;
+                $post->last_post->created_date = \DateTime::createFromFormat('U', $post->last_post->created / 1000);
+                $post->url = $this->config['url'] . '/messages/boards/thread/' . $post->id;
+
+                return $post;
+            },
+            $posts->getData()
+        );
+    }
+}
--- src/Controller/MeetupController.php
+++ src/Controller/MeetupController.php
@@ -3,6 +3,7 @@
 namespace PHPSW\Controller;
 
 use DMS\Service\Meetup\MeetupKeyAuthClient,
+    PHPSW\API\Meetup,
     Silex\Application,
     Symfony\Component\HttpFoundation\Request;
 
@@ -10,67 +11,22 @@ class MeetupController
 {
     public function eventsAction(Request $request, Application $app)
     {
-        $meetup = MeetupKeyAuthClient::factory(['key' => $app['meetup']['api']['key']]);
-
-        $events = $meetup->getEvents([
-            'group_urlname' => $app['meetup']['urlname'],
-            'status' => implode(',', [
-                'upcoming', 'past', 'proposed', 'suggested', 'cancelled'
-            ]),
-            'desc' => 'true'
-        ]);
-
-        $events = array_map(
-            function ($event) {
-                $event = (object) $event;
-
-                $event->date = \DateTime::createFromFormat('U', $event->time / 1000);
-                $event->url = $event->event_url;
-                $event->venue = (object) $event->venue;
-
-                return $event;
-            },
-            iterator_to_array($events)
-        );
+        $meetup = new Meetup($app['meetup']);
 
         return $app['twig']->render('meetup/events.html.twig', [
-            'events' => $events
+            'events' => $meetup->getEvents(),
+            'past_events' => $meetup->getPastEvents(),
+            'upcoming_events' => $meetup->getUpcomingEvents()
         ]);
     }
 
     public function postsAction(Request $request, Application $app)
     {
-        $meetup = MeetupKeyAuthClient::factory(['key' => $app['meetup']['api']['key']]);
-
-        $boards = $meetup->getDiscussionBoards([
-            'urlname' => $app['meetup']['urlname']
-        ]);
-
-        $board = (object) current($boards->getData());
-
-        $posts = $meetup
-            ->getDiscussions([
-                'urlname' => $app['meetup']['urlname'],
-                'bid' => $board->id
-            ])
-        ;
-
-        $posts = array_map(
-            function ($post) use ($app) {
-                $post = (object) $post;
-
-                $post->last_post = (object) $post->last_post;
-                $post->last_post->created_date = \DateTime::createFromFormat('U', $post->last_post->created / 1000);
-                $post->url = $app['meetup']['url'] . '/messages/boards/thread/' . $post->id;
-
-                return $post;
-            },
-            $posts->getData()
-        );
+        $meetup = new Meetup($app['meetup']);
 
         return $app['twig']->render('meetup/posts.html.twig', [
-            'boards' => $boards,
-            'posts' => array_slice($posts, 0, 3),
+            'boards' => $meetup->getDiscussionBoards(),
+            'posts' => array_slice($meetup->getPosts(), 0, 3),
         ]);
     }
 }
