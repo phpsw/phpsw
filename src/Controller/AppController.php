cad4f707c652f327cdb4ea9a1a29cd1566f2fe4d
Steve Lacey <steve@stevelacey.net>
2014-03-02 17:46:07 +0000

    Basic Twitter integration.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,7 +5,8 @@ namespace PHPSW\Controller;
 use DateTime,
     DMS\Service\Meetup\MeetupKeyAuthClient,
     Silex\Application,
-    Symfony\Component\HttpFoundation\Request;
+    Symfony\Component\HttpFoundation\Request,
+    Twitter;
 
 class AppController
 {
@@ -60,10 +61,66 @@ class AppController
             iterator_to_array($events)
         );
 
+        $twitter = new Twitter(
+            $app['twitter']['api']['key'],
+            $app['twitter']['api']['secret'],
+            $app['twitter']['access_token'],
+            $app['twitter']['access_token_secret']
+        );
+
+        $tweets = $twitter->load(Twitter::ME);
+
+        $tweets = array_map(
+            function ($tweet) {
+                $tweet->url = 'https://twitter.com/' . $tweet->user->screen_name . '/status/' . $tweet->id;
+
+                // create xhtml safe text (mostly to be safe of ampersands)
+                $tweet->html = htmlentities(html_entity_decode($tweet->text, ENT_NOQUOTES, 'UTF-8'), ENT_NOQUOTES, 'UTF-8');
+
+                // for tweets, let's extract the urls from the entities object
+                foreach ($tweet->entities->urls as $url) {
+                    $old_url        = $url->url;
+                    $expanded_url   = (empty($url->expanded_url))   ? $url->url : $url->expanded_url;
+                    $display_url    = (empty($url->display_url))    ? $url->url : $url->display_url;
+                    $replacement    = '<a href="' . $expanded_url . '" rel="external">' . $display_url . '</a>';
+                    $tweet->html    = str_replace($old_url, $replacement, $tweet->html);
+                }
+
+                // let's extract the hashtags from the entities object
+                foreach ($tweet->entities->hashtags as $hashtags) {
+                    $hashtag        = '#' . $hashtags->text;
+                    $replacement    = '<a href="https://twitter.com/search?q=%23'.$hashtags->text.'" rel="external">'.$hashtag.'</a>';
+                    $tweet->html    = str_ireplace($hashtag, $replacement, $tweet->html);
+                }
+
+                // let's extract the usernames from the entities object
+                foreach ($tweet->entities->user_mentions as $user_mentions) {
+                    $username       = '@'.$user_mentions->screen_name;
+                    $replacement    = '<a href="https://twitter.com/' . $user_mentions->screen_name . '" rel="external" title="' . $user_mentions->name . ' on Twitter">' . $username . '</a>';
+                    $tweet->html    = str_ireplace($username, $replacement, $tweet->html);
+                }
+
+                // if we have media attached, let's extract those from the entities as well
+                if (isset($tweet->entities->media)) {
+                    foreach ($tweet->entities->media as $media) {
+                        $old_url        = $media->url;
+                        $replacement    = '<a href="' . $media->expanded_url . '" rel="external" class="twitter-media" data-media="' . $media->media_url . '">' . $media->display_url . '</a>';
+                        $tweet->html    = str_replace($old_url, $replacement, $tweet->html);
+                    }
+                }
+
+                $tweet->created_date = new DateTime($tweet->created_at);
+
+                return $tweet;
+            },
+            $tweets
+        );
+
         return $app['twig']->render('index.html.twig', [
             'boards' => $boards,
             'events' => $events,
-            'posts' => array_slice($posts, 0, 3)
+            'posts' => array_slice($posts, 0, 3),
+            'tweets' => $tweets
         ]);
     }
 }
