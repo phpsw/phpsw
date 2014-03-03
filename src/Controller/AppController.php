458a18dfd7c8e73b6dbde931f80f487c361fd80d
Steve Lacey <steve@stevelacey.net>
2014-03-03 23:35:35 +0000

    Some pretty advanced caching.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,12 +3,15 @@
 namespace PHPSW\Controller;
 
 use Silex\Application,
-    Symfony\Component\HttpFoundation\Request;
+    Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\HttpFoundation\Response;
 
 class AppController
 {
     public function indexAction(Request $request, Application $app)
     {
-        return $app['twig']->render('index.html.twig');
+        return new Response($app['twig']->render('index.html.twig'), 200, [
+            'Cache-Control' => 's-maxage=3600'
+        ]);
     }
 }
