160eb56b4396be17f98c2810f95955980c7bbf30
Steve Lacey <steve@stevelacey.net>
2014-03-15 15:37:21 +0000

    Redis dump/restore commands.

---
+++ src/Command/Redis/DumpCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace PHPSW\Command\Redis;
+
+use Knp\Command\Command,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface,
+    Symfony\Component\Filesystem\Filesystem;
+
+class DumpCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('redis:dump-fixtures');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $app = $this->getSilexApplication();
+
+        $fixtures = $app['console.project_directory'] . '/fixtures';
+        $redis = $app['redis'];
+
+        $this->fs = new Filesystem;
+        $this->fs->exists($fixtures) or $this->fs->mkdir($fixtures);
+
+        $keys = $redis->keys('phpsw:*');
+
+        sort($keys);
+
+        foreach ($keys as $key) {
+            echo $key . ': ';
+
+            switch ($redis->type($key)) {
+                case 'hash':
+                    $dir = $fixtures . '/' . $key;
+                    $this->fs->exists($dir) or $this->fs->mkdir($dir);
+
+                    foreach ($redis->hgetall($key) as $key => $value) {
+                        $this->write($dir . '/' . $key, $value);
+                    }
+
+                    break;
+
+                case 'string':
+                    $this->write($fixtures . '/' . $key, $redis->get($key));
+            }
+
+            echo PHP_EOL;
+        }
+    }
+
+    protected function write($key, $value)
+    {
+        $this->fs->dumpFile($key, json_encode(json_decode($value), JSON_PRETTY_PRINT));
+
+        echo '.';
+    }
+}
---
+++ src/Command/Redis/RestoreCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace PHPSW\Command\Redis;
+
+use Knp\Command\Command,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface,
+    Symfony\Component\Finder\Finder;
+
+class RestoreCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('redis:restore-fixtures');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $app = $this->getSilexApplication();
+
+        $fixtures = $app['console.project_directory'] . '/fixtures';
+        $redis = $app['redis'];
+
+        foreach (Finder::create()->depth(0)->in($fixtures) as $node) {
+            echo $node->getFilename() . ': ';
+
+            if ($node->isDir()) {
+                $hash = $node->getFilename();
+
+                foreach (Finder::create()->files()->in($node->getPathname()) as $file) {
+                    $key = $file->getFilename();
+
+                    $redis->hset($hash, $key, $this->parse($file->getContents()));
+
+                    echo '.';
+                }
+            } else {
+                $redis->set($node->getFilename(), $this->parse($node->getContents()));
+
+                echo '.';
+            }
+
+            echo PHP_EOL;
+        }
+    }
+
+    protected function parse($json)
+    {
+        return json_encode(json_decode($json));
+    }
+}
