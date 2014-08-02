68406cff0ab16fe57863beb8a4d55230413f17a8
Steve Lacey <steve@stevelacey.net>
2014-08-02 21:30:04 +0100

    Refactored app controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -10,146 +10,66 @@ class AppController
 {
     public function indexAction(Application $app, Request $request)
     {
-        $communities = [
-            (object) [
-                'name' => 'Bristol Web Folk',
-                'logo' => 'bristol-web-folk',
-                'url'  => 'http://bristolwebfolk.github.io'
-            ],
-            (object) [
-                'name' => 'Bristol Drupal Group',
-                'logo' => 'drupal-bristol',
-                'url'  => 'https://groups.drupal.org/bristol-and-west-uk'
-            ],
-            (object) [
-                'name' => 'BrightonPHP',
-                'logo' => 'brighton-php',
-                'url'  => 'http://brightonphp.org'
-            ],
-            (object) [
-                'name' => 'PHP Dorset',
-                'logo' => 'php-dorset',
-                'url'  => 'http://www.phpdorset.co.uk'
-            ],
-            (object) [
-                'name' => 'PHP London',
-                'logo' => 'php-london',
-                'url'  => 'http://phplondon.org'
-            ],
-            (object) [
-                'name' => 'PHP Hampshire',
-                'logo' => 'php-hampshire',
-                'url'  => 'http://phphants.co.uk'
-            ],
-            (object) [
-                'name' => 'PHP Women : An inclusive &amp; global support network',
-                'logo' => 'php-women',
-                'url'  => 'http://phpwomen.org'
-            ],
-            (object) [
-                'name' => 'PHP Cambridge',
-                'logo' => 'php-cambridge',
-                'url'  => 'http://www.meetup.com/phpcambridge'
-            ],
-            (object) [
-                'name' => 'Leeds PHP',
-                'logo' => 'leed-php',
-                'url'  => 'http://leedsphp.org'
-            ],
-            (object) [
-                'name' => 'PHP East Midlands',
-                'logo' => 'phpem',
-                'url'  => 'http://phpem.info'
-            ],
-            (object) [
-                'name' => 'PHP North East',
-                'logo' => 'phpne',
-                'url'  => 'http://phpne.org.uk'
-            ],
-            (object) [
-                'name' => 'PHP North West',
-                'logo' => 'phpnw',
-                'url'  => 'http://conference.phpnw.org.uk'
-            ],
-            (object) [
-                'name' => 'PHP West Midlands',
-                'logo' => 'phpwm',
-                'url'  => 'http://phpwm.org.uk'
-            ],
-            (object) [
-                'name' => 'PHP UK',
-                'logo' => 'php-uk',
-                'url'  => 'http://phpconference.co.uk'
-            ]
-        ];
-
-        $body = $app['twig']->render('index.html.twig', [
-            'communities' => $communities,
-            'meetup' => $app['meetup.client']
-        ]);
-
-        return new Response($body, 200, ['Cache-Control' => 's-maxage=3600']);
+        return $this->render($app, 'index.html.twig');
     }
 
     public function brandAction(Application $app, Request $request)
     {
-        return new Response(
-            $app['twig']->render('brand.html.twig'),
-            200,
-            ['Cache-Control' => 's-maxage=3600']
-        );
+        return $this->render($app, 'brand.html.twig');
     }
 
     public function eventsAction(Application $app, Request $request)
     {
-        return new Response(
-            $app['twig']->render('events.html.twig', [
-                'meetup' => $app['meetup.client']
-            ]),
-            200,
-            ['Cache-Control' => 's-maxage=3600']
-        );
+        $statuses = ['upcoming', 'past'];
+
+        if ($app['debug']) {
+            array_unshift($statuses, 'draft');
+        }
+
+        $draftEvents = $app['meetup.client']->getDraftEvents();
+        $pastEvents = $app['meetup.client']->getPastEvents();
+        $upcomingEvents = $app['meetup.client']->getUpcomingEvents();
+
+        return $this->render($app, 'events.html.twig', [
+            'next_event' => array_shift($upcomingEvents),
+            'statuses' => $statuses,
+            'draft_events' => $draftEvents,
+            'past_events' => $pastEvents,
+            'upcoming_events' => $upcomingEvents
+        ]);
     }
 
     public function eventAction(Application $app, Request $request, $id)
     {
         $event = $app['meetup.client']->getEvent($id);
 
-        if ($event) {
-            $response = new Response(
-                $app['twig']->render('event.html.twig', [
-                    'meetup' => $app['meetup.client'],
-                    'event' => $event
-                ]),
-                200,
-                ['Cache-Control' => 's-maxage=3600']
-            );
-        } else {
+        if (!$event) {
             throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
         }
 
-        return $response;
+        return $this->render($app, 'event.html.twig', [
+            'event' => $event
+        ]);
     }
 
     public function speakersAction(Application $app, Request $request)
     {
-        return new Response(
-            $app['twig']->render('speakers.html.twig', [
-                'meetup' => $app['meetup.client']
-            ]),
-            200,
-            ['Cache-Control' => 's-maxage=3600']
-        );
+        return $this->render($app, 'speakers.html.twig');
     }
 
     public function sponsorsAction(Application $app, Request $request)
     {
+        return $this->render($app, 'sponsors.html.twig');
+    }
+
+    protected function render($app, $template, $parameters = [])
+    {
+        $parameters['meetup'] = $app['meetup.client'];
+
         return new Response(
-            $app['twig']->render('sponsors.html.twig', [
-                'meetup' => $app['meetup.client']
-            ]),
-            200,
-            ['Cache-Control' => 's-maxage=3600']
+            $app['twig']->render($template, $parameters), 200, [
+                'Cache-Control' => 's-maxage=3600'
+            ]
         );
     }
 }
