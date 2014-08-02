30f2e96be21037fb1b45a456427ad311e5cf01e0
Steve Lacey <steve@stevelacey.net>
2014-08-02 21:29:07 +0100

    Adjusted cap composer tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,18 +5,16 @@ set :repo_url,  "git@github.com:phpsw/#{fetch(:application)}.git"
 set :linked_files, %w{config/secrets.yml}
 
 namespace :composer do
-  desc "Copy vendors from previous release"
-  task :copy_vendors do
-    on roles :all, reject: lambda { |h| h.properties.no_release } do
-      last_release = releases_path.join(capture(:ls, '-xr', releases_path).split[1])
-      execute "if [ -d #{last_release}/vendor ]; then cp -a #{last_release}/vendor #{release_path}/vendor; fi"
-    end
-  end
-
-  desc "Install"
   task :install do
     on roles :all do
       execute "composer install --no-dev --optimize-autoloader --prefer-source --working-dir #{release_path} --verbose"
+    end
+  end
+
+  before :install, :copy do
+    on roles :all, reject: lambda { |h| h.properties.no_release } do
+      last_release = releases_path.join(capture(:ls, '-xr', releases_path).split[1])
+      execute "if [ -d #{last_release}/vendor ]; then cp -a #{last_release}/vendor #{release_path}/vendor; fi"
     end
   end
 end
