b062ff5b46e4c88b6b8d3c5f3d032b6a8c0309e0
Steve Lacey <steve@stevelacey.net>
2014-08-04 00:03:22 +0100

    Deploy fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,62 @@ namespace :composer do
       execute "if [ -d #{last_release}/vendor ]; then cp -a #{last_release}/vendor #{release_path}/vendor; fi"
     end
   end
+
+  after "deploy:updating", :install
+end
+
+namespace :npm do
+  task :install do
+    on roles :app do
+      within release_path do
+        execute :npm, :install, "--quiet"
+      end
+    end
+  end
+
+  before :install, :copy do
+    on roles :app, reject: lambda { |h| h.properties.no_release } do
+      last_release = releases_path.join(capture(:ls, "-xr", releases_path).split[1])
+      execute "if [ -d #{last_release}/node_modules ]; then cp -a #{last_release}/node_modules #{release_path}/node_modules; fi"
+    end
+  end
+
+  after "deploy:updating", :install
+end
+
+namespace :bower do
+  task :install do
+    on roles :app do
+      within release_path do
+        with path: "#{release_path}/node_modules/.bin:$PATH" do
+          execute :bower, :install, "--quiet"
+        end
+      end
+    end
+  end
+
+  before :install, :copy do
+    on roles :app, reject: lambda { |h| h.properties.no_release } do
+      last_release = releases_path.join(capture(:ls, "-xr", releases_path).split[1])
+      execute "if [ -d #{last_release}/vendor ]; then cp -a #{last_release}/vendor #{release_path}/vendor; fi"
+    end
+  end
+
+  after "npm:install", :install
+end
+
+namespace :gulp do
+  task :build do
+    on roles :app do
+      within release_path do
+        with node_env: fetch(:env), path: "#{release_path}/node_modules/.bin:$PATH" do
+          execute :gulp, :build
+        end
+      end
+    end
+  end
+
+  after "bower:install", :build
 end
 
 namespace :mod_pagespeed do
@@ -25,6 +81,8 @@ namespace :mod_pagespeed do
       execute "sudo touch /var/cache/mod_pagespeed/cache.flush"
     end
   end
+
+  after "deploy:finishing", :flush
 end
 
 namespace :varnish do
@@ -33,9 +91,6 @@ namespace :varnish do
       execute "sudo service varnish restart"
     end
   end
-end
 
-after "deploy:updating", "composer:copy_vendors"
-after "deploy:updating", "composer:install"
-after "deploy:finishing", "mod_pagespeed:flush"
-after "deploy:finishing", "varnish:restart"
+  after "deploy:finishing", :restart
+end
