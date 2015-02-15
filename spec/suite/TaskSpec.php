da0e009e154e8595943cb3c532ac4aea1e462054
Steve Lacey <steve@stevelacey.net>
2015-02-15 23:56:23 +0000

    Task specs

---
+++ spec/suite/TaskSpec.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace PHPSW\Spec\Suite;
+
+describe("Task", function () {
+    describe("meetup:import:all", function () {
+        it("expects that the task runs", function () {
+            $stdout = task('meetup:import:all');
+
+            expect($stdout)->toMatch(trimlns('#Group: \.
+                Events: \.+
+                Photos: \.+
+                Posts: \.+
+                Reviews: \.+
+                Members: \.+
+                Speakers: \.+
+                Talks: \.+
+            #'));
+        });
+    });
+
+    describe("redis:restore-fixtures", function () {
+        it("expects that the task runs", function () {
+            $stdout = task('redis:restore-fixtures');
+
+            expect($stdout)->toMatch(trimlns('#events: \.+
+                group: \.
+                members: \.+
+                photos: \.+
+                posts: \.+
+                reviews: \.+
+                slides: \.+
+                speakers: \.+
+                talks: \.+
+            #'));
+        });
+    });
+
+    describe("twitter:import:all", function () {
+        it("expects that the task runs", function () {
+            $stdout = task('twitter:import:all');
+
+            expect($stdout)->toMatch('#Tweets: \.+\n#');
+        });
+    });
+
+    afterEach(function () {
+        global $redis;
+
+        $redis->del('events');
+        $redis->del('group');
+        $redis->del('members');
+        $redis->del('photos');
+        $redis->del('posts');
+        $redis->del('reviews');
+        $redis->del('slides');
+        $redis->del('speakers');
+        $redis->del('talks');
+    });
+});
