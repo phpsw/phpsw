5b1dbde141c2c586000712e572e4d0291c42b86c
Steve Lacey <steve@stevelacey.net>
2015-11-22 15:31:59 +0000

    Blog upstart, various fixes, switched to private ips and 0.0.0.0 listens to support multiple app servers, added app2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,12 @@
-server "52.30.232.72",
+server "app1.phpsw.uk",
+  user: "ubuntu",
+  roles: %w[app web],
+  ssh_options: {
+    forward_agent: true,
+    auth_methods: %w[publickey]
+  }
+
+server "app2.phpsw.uk",
   user: "ubuntu",
   roles: %w[app web],
   ssh_options: {
